93b682ff43f7816056ecba6b0fe0ca4d49e84d55
alefbispo <Blacksabbath21@>
2022-05-20 18:57:49 -0300

    criando corpo

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(ExpensesaApp());
+}
+
+class ExpensesaApp extends StatelessWidget {
+  const ExpensesaApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Center(
+        child: Text('Versão Inicial'),
+      ),
+    );
+  }
+}
