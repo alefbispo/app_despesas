83a398e616131275f0a39dedc45bcc6bcc91f162
alefbispo <Blacksabbath21@>
2022-05-22 22:21:46 -0300

    imagem para lista vazia com operador ternario

--- lib/models/transation_list.dart
+++ lib/models/transation_list.dart
@@ -10,54 +10,71 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 450,
-      child: ListView.builder(
-        itemCount: transations.length,
-        itemBuilder: (context, index) {
-          final tr = transations[index];
-          return Card(
-            elevation: 5,
-            child: Row(children: [
-              Container(
-                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    border: Border.all(
-                      color: Theme.of(context).primaryColor,
-                      width: 2,
-                    ),
+      height: 300,
+      child: transations.isEmpty
+          ? Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Nenuma transação cadastrada!',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                SizedBox(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
                   ),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    'R\$ ${tr.value.toStringAsFixed(2)}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of(context).primaryColor),
-                  )),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    tr.title,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    DateFormat('dd/MM/yyy').format(tr.date),
-                    style: TextStyle(
-                      fontWeight: FontWeight.w100,
-                    ),
-                  ),
-                ],
-              )
-            ]),
-          );
-        },
-      ),
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemCount: transations.length,
+              itemBuilder: (context, index) {
+                final tr = transations[index];
+                return Card(
+                  elevation: 5,
+                  child: Row(children: [
+                    Container(
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          border: Border.all(
+                            color: Theme.of(context).primaryColor,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'R\$ ${tr.value.toStringAsFixed(2)}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context).primaryColor),
+                        )),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tr.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          DateFormat('dd/MM/yyy').format(tr.date),
+                          style: TextStyle(
+                            fontWeight: FontWeight.w100,
+                          ),
+                        ),
+                      ],
+                    )
+                  ]),
+                );
+              },
+            ),
     );
   }
 }
