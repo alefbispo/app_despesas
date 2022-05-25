e2a17ab748f63d5af19e09282140d1d79c85ab83
alefbispo <Blacksabbath21@>
2022-05-24 21:05:05 -0300

    substituindo lista com row por list title

--- lib/components/transation_list.dart
+++ lib/components/transation_list.dart
@@ -33,45 +33,27 @@ class TransactionList extends StatelessWidget {
                 final tr = transations[index];
                 return Card(
                   elevation: 5,
-                  child: Row(children: [
-                    Container(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          border: Border.all(
-                            color: Theme.of(context).colorScheme.secondary,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          'R\$ ${tr.value.toStringAsFixed(2)}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Theme.of(context).colorScheme.secondary),
-                        )),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          tr.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 5),
-                        Text(
-                          DateFormat('dd/MM/yyy').format(tr.date),
-                          style: TextStyle(
-                            fontWeight: FontWeight.w100,
-                          ),
-                        ),
-                      ],
-                    )
-                  ]),
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(child: Text('R\$${tr.value}')),
+                      ),
+                    ),
+                    title: Text(
+                      tr.title,
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    subtitle: Text(
+                      DateFormat('d/MM/y').format(tr.date),
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                  ),
                 );
               },
             ),
